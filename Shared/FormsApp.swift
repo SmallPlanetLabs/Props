d27defc86227a077ef820dbd3e7ed2c9854b1e3d
Quinn McHenry <quinn@jqm.us>
2021-11-01 09:37:07 -0400

    Initial Commit

---
+++ Shared/FormsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormsApp.swift
+//  Shared
+//
+//  Created by Quinn McHenry on 11/1/21.
+//
+
+import SwiftUI
+
+@main
+struct FormsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
