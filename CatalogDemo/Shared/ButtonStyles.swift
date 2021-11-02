ba9faeb9a1b0baf39befa4cbac0e55104bb796c0
Quinn McHenry <quinn@jqm.us>
2021-11-02 14:51:49 -0400

    Add fancy new button and organize

--- CatalogDemo/Shared/ButtonStyles.swift
+++ CatalogDemo/Shared/ButtonStyles.swift
@@ -12,62 +12,101 @@ struct ButtonStyles_Previews: PreviewProvider {
 
     static var previews: some View {
         ScrollView {
-            VStack(spacing: 20) {
+            VStack(spacing: 12) {
+                Group {
+                    Divider()
+                    Text("Custom ButtonStyles").font(.title)
+
+                    Group {
+                        EnabledAndDisabled(title: "FilledButton") {
+                            Button("Filled Button") {}
+                        }
+
+                        EnabledAndDisabled (title: nil) {
+                            Button { } label: {
+                                HStack { Text("Filled Button").fixedSize(); Image(systemName: "greaterthan") }
+                            }
+                        }
+
+                        EnabledAndDisabled(title: nil) {
+                            Button { } label: { Image(systemName: "greaterthan") }
+                        }
+                    }
+                    .buttonStyle(FilledButton())
+
+                    Group {
+                        EnabledAndDisabled(title: "OutlinedButton") {
+                            Button("OutlinedButton") {}
+                        }
+
+                        EnabledAndDisabled (title: nil) {
+                            Button { } label: {
+                                HStack(spacing: 12) { Text("Outlined Button").fixedSize(); Image(systemName: "greaterthan") }
+                            }
+                        }
+
+                        EnabledAndDisabled(title: nil) {
+                            Button { } label: { Image(systemName: "greaterthan") }
+                        }
+                    }
+                    .buttonStyle(OutlinedButton())
+
+                    Group {
+                        EnabledAndDisabled(title: "FanciedButton") {
+                            Button("Button") {}
+                        }
+                    }
+                    .buttonStyle(FanciedButton())
+                    .font(.custom("ArialRoundedMTBold", fixedSize: 24))
+
+                }
+                .accentColor(.blue)
+
                 Group {
                     Text("System ButtonStyles").font(.title)
-                    EnabledAndDisabled {
+                    EnabledAndDisabled(title: "Unstyled Button") {
                         Button("Unstyled Button") {}
                     }
 
-                    EnabledAndDisabled {
+                    EnabledAndDisabled(title: "DefaultButtonStyle") {
                         Button("DefaultButtonStyle") {}.buttonStyle(DefaultButtonStyle())
                     }
 
                     if #available(iOS 15.0, *) {
-                        EnabledAndDisabled {
+                        EnabledAndDisabled(title: "BorderedButtonStyle iOS 15+)") {
                             Button("BorderedButtonStyle") {}.buttonStyle(BorderedButtonStyle())
                         }
 
-                        EnabledAndDisabled {
+                        EnabledAndDisabled(title: "BorderedProminentButtonStyle (iOS 15+") {
                             Button("BorderedProminentButtonStyle") {}.buttonStyle(BorderedProminentButtonStyle())
                         }
                         .tint(.orange)
 
                     }
 
-                    EnabledAndDisabled {
+                    EnabledAndDisabled(title: "BorderlessButtonStyle") {
                         Button("BorderlessButtonStyle") {}.buttonStyle(BorderlessButtonStyle())
                     }
 
-                    EnabledAndDisabled {
+                    EnabledAndDisabled(title: "PlainButtonStyle") {
                         Button("PlainButtonStyle") {}.buttonStyle(PlainButtonStyle())
                     }
                 }
 
-                Group {
-                    Text("Custom ButtonStyles").font(.title)
-
-                    EnabledAndDisabled {
-                        Button("Custom OutlineButton") {}.buttonStyle(OutlineButton())
-                    }
-
-                    EnabledAndDisabled {
-                        Button("FilledButton") {}.buttonStyle(FilledButton())
-                    }
-                    .accentColor(.blue)
-                }
-
             }
-            .accentColor(.green)
-        .padding()
+            .padding()
+            .padding(.bottom, 100)
+        .previewLayout(.sizeThatFits)
         }
     }
 }
 
-struct OutlineButton: ButtonStyle {
+// TODO: add an Environment value for corner radius
+
+struct OutlinedButton: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
-        let background = RoundedRectangle(cornerRadius: 8,style: .continuous)
-                .stroke(configuration.isPressed ? .gray : Color.accentColor)
+        let background = RoundedRectangle(cornerRadius: 2, style: .continuous)
+            .stroke(configuration.isPressed ? .gray : Color.accentColor)
 
         configuration
             .label
@@ -79,8 +118,8 @@ struct OutlineButton: ButtonStyle {
 
 struct FilledButton: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
-        let background = RoundedRectangle(cornerRadius: 0, style: .continuous)
-                .fill(configuration.isPressed ? .gray : Color.accentColor)
+        let background = RoundedRectangle(cornerRadius: 2, style: .continuous)
+            .fill(configuration.isPressed ? .gray : Color.accentColor)
 
         configuration
             .label
@@ -90,15 +129,70 @@ struct FilledButton: ButtonStyle {
     }
 }
 
+struct FanciedButton: ButtonStyle {
+    @Environment(\.isEnabled) var isEnabled: Bool
+    func makeBody(configuration: Configuration) -> some View {
+        let radius = radius(for: configuration)
+        let xy = offset(for: configuration)
+        let background = Capsule()
+            .fill(color(for: configuration))
+            .shadow(color: Color(white: 0.6), radius: radius, x: xy, y: xy)
+
+        configuration
+            .label
+            .padding()
+            .background(background)
+            .foregroundColor(.accentColor.opacity(isEnabled ? 1 : 0.4))
+
+    }
+
+    func color(for configuration: Configuration) -> Color {
+        if configuration.isPressed {
+            return Color(white: 0.95, opacity: 1)
+        } else {
+            return Color(white: 0.95, opacity: 1)
+        }
+    }
+
+    func radius(for configuration: Configuration) -> CGFloat {
+        if configuration.isPressed {
+            return 6
+        } else if isEnabled {
+            return 12
+        } else {
+            return 2
+        }
+    }
+
+    func offset(for configuration: Configuration) -> CGFloat {
+        if configuration.isPressed {
+            return 6
+        } else if isEnabled {
+            return 12
+        } else {
+            return 2
+        }
+    }
+}
+
 // MARK: - Helpers
 
 struct EnabledAndDisabled<Content>: View where Content: View {
+    let title: String?
     @ViewBuilder var content: Content
 
     var body: some View {
-        HStack {
-            content
-            content.disabled(true)
+        VStack(spacing: 10) {
+            if let title = title {
+                HStack {
+                    Text(title).font(.caption)
+                    Spacer()
+                }
+            }
+            HStack {
+                content
+                content.disabled(true)
+            }
         }
     }
 }
