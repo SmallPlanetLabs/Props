513e95cb25b58cd4e9b7f0d31a670e0b103d0b85
Quinn McHenry <quinn@jqm.us>
2021-11-02 10:43:04 -0400

    Add new Catalog project

---
+++ Catalog/Shared/Catalog.swift
@@ -0,0 +1,21 @@
+//
+//  Catalog.swift
+//  Shared
+//
+//  Created by Quinn McHenry on 11/2/21.
+//
+
+import SwiftUI
+
+struct Catalog: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Catalog()
+    }
+}
---
+++ Catalog/Shared/CatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatalogApp.swift
+//  Shared
+//
+//  Created by Quinn McHenry on 11/2/21.
+//
+
+import SwiftUI
+
+@main
+struct CatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Catalog()
+        }
+    }
+}
