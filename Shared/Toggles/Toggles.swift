7d7c0d8e0ef997acf21ff4ba0e8be054b1d5d43f
Quinn McHenry <quinn.mchenry@smallplanet.com>
2021-11-04 00:05:28 -0400

    Add 3rd party toggle style (sun/moon) to catalog app

--- Shared/Toggles/Toggles.swift
+++ Shared/Toggles/Toggles.swift
@@ -6,11 +6,19 @@
 //
 
 import SwiftUI
+import SunMoonToggleStyle
 
 struct Toggles: View {
     var body: some View {
         ScrollView {
             VStack(spacing: 30) {
+                Text("Custom Toggle Styles").font(.title)
+
+                ToggleExample(title: "SunMoonToggleStyle (3rd party)") {
+                    Text("Sun/moon")
+                }
+                .toggleStyle(SunMoonToggleStyle())
+
                 Text("System Toggle Styles").font(.title)
                 SystemToggleStyles()
 
