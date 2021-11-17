65c9a8b560da149efcef16d81248ff69286217a8
Quinn McHenry <quinn.mchenry@smallplanet.com>
2021-11-17 15:32:14 -0500

    Add preview device/config combos to MultipleDevices [WIP]
    Pending backporting orientation to ignore iOS 14

--- Props/Sources/Props/Utilities/MultipleDevices.swift
+++ Props/Sources/Props/Utilities/MultipleDevices.swift
@@ -13,29 +13,79 @@ import SwiftUI
 /// `iPhone 8`, `iPhone 8`,
 /// and `iPod touch (7th generation)`.
 public struct MultipleDevices<Content: View>: View {
-    let enabled: Bool
+    let combos: [PreviewCombo]
     let content: Content
 
-    public init(enabled: Bool = true, @ViewBuilder content: () -> Content) {
-        self.enabled = enabled
+    public init(combos: MultipleDeviceCombo = .defaults, @ViewBuilder content: () -> Content) {
+        self.combos = combos
         self.content = content()
     }
 
     public var body: some View {
-        Group {
-            if enabled {
-                content
-                    .previewDevice("iPhone 13 Pro Max")
-                    .previewDisplayName("iPhone 13 Pro Max")
-                content
-                    .previewDevice("iPhone 8")
-                    .previewDisplayName("iPhone 8")
-                content
-                    .previewDevice("iPod touch (7th generation)")
-                    .previewDisplayName("iPod touch (7th generation)")
-            } else {
-                content
-            }
+        ForEach(combos, id: \.name) { combo in
+            content
+                .previewDevice(SwiftUI.PreviewDevice(rawValue: combo.device.name))
+                .previewDisplayName(combo.name)
+                .colorScheme(combo.colorScheme)
+                // .previewInterfaceOrientation(combo.orientation)
+        }
+    }
+}
+
+public struct PreviewCombo {
+    let device: PreviewDevice
+    let colorScheme: ColorScheme
+    // let orientation: InterfaceOrientation
+
+    var name: String {
+        "\(device.name) \(colorScheme)" //" \(orientation)"
+    }
+
+    public init(device: PreviewDevice, scheme: ColorScheme = .light) { // }, orientation: InterfaceOrientation = .portrait) {
+        self.device = device
+        self.colorScheme = scheme
+        // self.orientation = orientation
+    }
+}
+
+public typealias MultipleDeviceCombo = [PreviewCombo]
+
+extension MultipleDeviceCombo {
+    public static let defaults: [PreviewCombo] = [
+        PreviewCombo(device: .iPhone13ProMax),
+        PreviewCombo(device: .iPodTouch7),
+    ]
+
+    public static let iPhonesAndiPad: [PreviewCombo] = [
+        PreviewCombo(device: .iPhone13ProMax, scheme: .dark),
+        PreviewCombo(device: .iPadAir4),
+        PreviewCombo(device: .iPodTouch7),
+    ]
+
+//    public static let iPhoneOrientations: [PreviewCombo] = [
+//        PreviewCombo(device: .iPhone13ProMax),
+//        PreviewCombo(device: .iPhone13ProMax, orientation: .landscapeLeft),
+//        PreviewCombo(device: .iPhone13ProMax, orientation: .landscapeRight),
+//        PreviewCombo(device: .iPhone13ProMax, orientation: .portraitUpsideDown),
+//    ]
+}
+
+public enum PreviewDevice {
+    case iPadAir4
+    case iPhone13ProMax
+    case iPhone8
+    case iPodTouch7
+
+    var name: String {
+        switch self {
+        case .iPadAir4:
+            return "iPad Air (4th generation)"
+        case .iPhone13ProMax:
+            return "iPhone 13 Pro Max"
+        case .iPhone8:
+            return "iPhone 8"
+        case .iPodTouch7:
+            return "iPod touch (7th generation)"
         }
     }
 }
