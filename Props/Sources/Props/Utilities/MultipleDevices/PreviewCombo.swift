0d908348374524cc5cae9585d423e9d834597a3e
Quinn McHenry <quinn.mchenry@smallplanet.com>
2021-11-18 23:31:14 -0500

    Update multiple device preview refactor

--- Props/Sources/Props/Utilities/MultipleDevices/PreviewCombo.swift
+++ Props/Sources/Props/Utilities/MultipleDevices/PreviewCombo.swift
@@ -1,8 +1,24 @@
 //
-//  File.swift
-//  
+//  PreviewCombo.swift
+//  Props
 //
 //  Created by Quinn McHenry on 11/18/21.
 //
 
-import Foundation
+import SwiftUI
+
+public struct PreviewCombo {
+    let device: PreviewDevice
+    let colorScheme: ColorScheme
+    let orientation: InterfaceOrientation
+
+    var name: String {
+        "\(device.name) \(colorScheme) \(orientation)"
+    }
+
+    public init(device: PreviewDevice, scheme: ColorScheme = .light, orientation: InterfaceOrientation = .portrait) {
+        self.device = device
+        self.colorScheme = scheme
+        self.orientation = orientation
+    }
+}
